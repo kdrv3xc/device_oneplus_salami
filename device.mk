65e52f594aec033adf8c18f3b9bb77fba77f9b6a
primes <kdrv3xc@gmail.com>
2024-07-06 10:00:54 +0000

    i
    Change-Id: I0233d5adbe31eb254f95f39488b3ffb7bde7fa8e

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#PRODUCT_FULL_TREBLE_OVERRIDE := true
+-include vendor/lineage-priv/keys/keys.mk
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
